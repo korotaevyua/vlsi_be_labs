8b392be764b69043678115667aa9bd9bcd8a88a0
korotaevyua <korotaev240297@gmail.com>
2024-08-18 23:22:10 +0300

    fix pins place

--- scr/layout/pins_placement.tcl
+++ scr/layout/pins_placement.tcl
@@ -34,6 +34,7 @@ lappend output_pins [get_full_name $output_pin]
 
 set number_input_pins [llength $input_pins]
 set y_step_inputs [expr {double($Y_size) / double($number_input_pins + 1) }]
+set y_step_inputs [format "%.2f" $y_step_inputs]
 set y_inputs $y_step_inputs
 
 for {set i 0} {$i < $number_input_pins} {incr i} {
@@ -49,6 +50,7 @@ set y_inputs [expr $y_inputs + $y_step_inputs]
 
 set number_output_pins [llength $output_pins]
 set y_step_outputs [expr {double($Y_size) / double($number_output_pins + 1)}]
+set y_step_outputs [format "%.2f" $y_step_outputs]
 set y_outputs $y_step_outputs
 
 for {set i 0} {$i < $number_output_pins} {incr i} {
