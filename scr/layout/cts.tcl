3ec956bbee2a03363582e8c6b59bb31764a2e0cd
korotaevyua <korotaev240297@gmail.com>
2024-08-18 21:54:33 +0300

    modify pin placement

--- scr/layout/cts.tcl
+++ scr/layout/cts.tcl
@@ -3,31 +3,6 @@ set_routing_layers -signal met2-met3 -clock met2-met3
 set_wire_rc -clock -layer met3
 set_wire_rc -signal -layer met3
 
-##path groups
-group_path -name reg2reg -from [all_registers] -to [all_registers]
-group_path -name in2reg -from [all_inputs] -to [all_registers]
-group_path -name reg2out -from [all_registers] -to [all_outputs]
-group_path -name in2out -from [all_inputs] -to [all_outputs]
-
-##
-set_false_path -from [all_inputs] -to [all_registers]
-set_false_path -from [all_registers] -to [all_outputs]
-set_false_path -from [all_inputs] -to [all_outputs]
-
-#
-unset_propagated_clock [all_clocks]
-
-set_propagated_clock [all_clocks]
-
-set max_slew [expr 0.50 * 1e-9]; # must convert to seconds
-set max_cap  [expr 5 * 1e-12]; # must convert to farad
-
-repair_clock_inverters
-
-configure_cts_characterization\
-    -max_slew $max_slew\
-    -max_cap $max_cap
-    
 set ckbuf_list "sky130_fd_sc_hd__clkbuf_1 sky130_fd_sc_hd__clkbuf_2 sky130_fd_sc_hd__clkbuf_4"
 set root_ckbuf_list "sky130_fd_sc_hd__clkbuf_8"
 
--- scr/layout/pins_placement.tcl
+++ scr/layout/pins_placement.tcl
@@ -1,42 +1,64 @@
-set inputs [all_inputs]
+#Copyright (c) 2024 Yuri Korotaev
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
 set input_pins {}
-
-foreach input $inputs {
-lappend input_pins [get_full_name $input]
-}
-
-set outputs [all_outputs]
-
 set output_pins {}
 
-foreach output $outputs {
-lappend output_pins [get_full_name $output]
+set inputs [all_inputs]
+set outputs [all_outputs]
+
+foreach input_pin $inputs {
+lappend input_pins [get_full_name $input_pin]
 }
 
-set y 0
+foreach output_pin $outputs {
+lappend output_pins [get_full_name $output_pin]
+}
 
-for {set i 0} {$i < [llength $input_pins]} {incr i} {
+set number_input_pins [llength $input_pins]
+set y_step_inputs [expr {double($Y_size) / double($number_input_pins + 1) }]
+set y_inputs $y_step_inputs
+
+for {set i 0} {$i < $number_input_pins} {incr i} {
 
 place_pin \
 -pin_name [lindex $input_pins $i] \
 -layer met3 \
--location "0.23 $y" \
+-location "0.23 $y_inputs" \
 -pin_size {0.46 0.30}
-set y [expr $y + 2.72]
+set y_inputs [expr $y_inputs + $y_step_inputs]
 
 }
 
-set y $Y_size
+set number_output_pins [llength $output_pins]
+set y_step_outputs [expr {double($Y_size) / double($number_output_pins + 1)}]
+set y_outputs $y_step_outputs
 
-for {set i 0} {$i < [llength $output_pins]} {incr i} {
+for {set i 0} {$i < $number_output_pins} {incr i} {
 
 place_pin \
 -pin_name [lindex $output_pins $i] \
 -layer met3 \
--location "$X_size $y" \
+-location "$X_size $y_outputs" \
 -pin_size {0.46 0.30}
-set y [expr $y - 2.72]
+set y_outputs [expr $y_outputs + $y_step_outputs]
 
 }
 
